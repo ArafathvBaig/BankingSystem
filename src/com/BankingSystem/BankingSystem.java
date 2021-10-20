c82c02b653dcbbbd92061769f2f2a99d5e27dad9
“ArafathvBaig” <arafathbaig1997@gmail.com>
2021-10-20 18:56:39 +0530

    Adding Banking System File to the Branch

---
+++ src/com/BankingSystem/BankingSystem.java
@@ -0,0 +1,36 @@
+package com.BankingSystem;
+
+import java.util.Scanner;
+
+public class BankingSystem 
+{
+		static String bankName="State Bank of India";
+		long accNum;
+		String custName;
+		int custAge;
+		
+		public void display()
+		{
+			Scanner sc=new Scanner(System.in);
+			System.out.println("Enter the Customer Name: ");
+			custName=sc.nextLine();
+			System.out.println("Enter the Customer Age: ");
+			custAge=sc.nextInt();
+			System.out.println("Enter the Customer Account Number: ");
+			accNum=sc.nextLong();
+			System.out.println("Bank Name: "+bankName);
+			System.out.println("Name: "+custName);
+			System.out.println("Age: "+custAge);
+			System.out.println("Account Number: "+accNum);
+		}
+		
+		public static void main(String[] args) 
+		{
+			Scanner sc=new Scanner(System.in);
+			System.out.println("*************WELCOME TO SBI*************");
+			System.out.println("\n==============================================\n");
+			BankingSystem obj = new BankingSystem();
+			obj.display();
+
+		}
+}
